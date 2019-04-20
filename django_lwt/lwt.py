5a7b335e3f54032b52b235b77075624db575a87a
Alex Khatskevich <avkhatskevich@gmail.com>
2019-04-20 17:29:59 +0200

    Fix underline syntax

--- django_lwt/lwt.py
+++ django_lwt/lwt.py
@@ -10,7 +10,7 @@ from . import exceptions as BEx
 import json
 from django.contrib.auth import get_user_model
 
-__SETTINGS = {
+_SETTINGS = {
         'APP_VERSION': 0,
         # When the token cannot be trusted.
         'SIGNATURE_EXPIRATION': 60 * 60 * 24 * 365,
@@ -23,23 +23,23 @@ __SETTINGS = {
         'AUTH_HEADER_PREFIX': 'lwt',
         }
 
-__INITIALIZED = False
+_INITIALIZED = False
 
 
-def __init_settings():
-    global __INITIALIZED
-    if __INITIALIZED:
+def _init_settings():
+    global _INITIALIZED
+    if _INITIALIZED:
         return
     if not hasattr(settings, 'LWT_AUTHENTICATION'):
         return
     explicit_settings = settings.LWT_AUTHENTICATION
-    for key in __SETTINGS.keys():
+    for key in _SETTINGS.keys():
         if key in explicit_settings:
-            __SETTINGS[key] = explicit_settings[key]
-    uf = __SETTINGS['SAVE_USER_FIELDS']
+            _SETTINGS[key] = explicit_settings[key]
+    uf = _SETTINGS['SAVE_USER_FIELDS']
     uf = set(uf) - {'id', 'pk'}
-    __SETTINGS['SAVE_USER_FIELDS'] = list(uf)
-    __INITIALIZED = True
+    _SETTINGS['SAVE_USER_FIELDS'] = list(uf)
+    _INITIALIZED = True
 
 
 class BaseLWTAuthentication(BaseAuthentication):
@@ -48,7 +48,7 @@ class BaseLWTAuthentication(BaseAuthentication):
     """
 
     def __init__(self, *args, **kwargs):
-        __init_settings()
+        _init_settings()
         super().__init__(*args, **kwargs)
         self.bwt = BWT(settings.SECRET_KEY)
 
@@ -56,8 +56,8 @@ class BaseLWTAuthentication(BaseAuthentication):
         auth = get_authorization_header(request).split()
 
         if not auth:
-            return request.COOKIES.get(__SETTINGS['COOKIE_NAME'])
-        if auth[0].decode('utf-8').lower() != __SETTINGS['AUTH_HEADER_PREFIX']:
+            return request.COOKIES.get(_SETTINGS['COOKIE_NAME'])
+        if auth[0].decode('utf-8').lower() != _SETTINGS['AUTH_HEADER_PREFIX']:
             return None
         return auth[1]
 
@@ -66,7 +66,7 @@ class BaseLWTAuthentication(BaseAuthentication):
         if lwt_value is None:
             return None
         try:
-            issue_max_time = int(time.time()) - __SETTINGS['SIGNATURE_EXPIRATION']
+            issue_max_time = int(time.time()) - _SETTINGS['SIGNATURE_EXPIRATION']
             data = self.bwt.decode(lwt_value, issue_max_time)
         except BEx.BWTExpired:
             raise REx.AuthenticationFailed("Signature expired")
@@ -115,25 +115,25 @@ class LWTAuthentication(BaseLWTAuthentication):
         msg = {
                 'pk': pk,
                 }
-        for field in __SETTINGS['SAVE_USER_FIELDS']:
+        for field in _SETTINGS['SAVE_USER_FIELDS']:
             msg[field] = getattr(user, field)
         msg = json.dumps(
                 msg, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
         token = self.bwt.encode(
-                msg, app_version=__SETTINGS['APP_VERSION'])
+                msg, app_version=_SETTINGS['APP_VERSION'])
         return token
 
     def set_cookie(self, response, token):
-        response.set_cookie(__SETTINGS['COOKIE_NAME'], token,
-                            max_age=__SETTINGS['SIGNATURE_EXPIRATION'])
+        response.set_cookie(_SETTINGS['COOKIE_NAME'], token,
+                            max_age=_SETTINGS['SIGNATURE_EXPIRATION'])
 
     def get_user(self, data):
         user = self.get_blank_user(data['msg']['pk'])
         # Data is expired
-        if data['issue_time'] + __SETTINGS['DATA_EXPIRATION'] < time.time():
+        if data['issue_time'] + _SETTINGS['DATA_EXPIRATION'] < time.time():
             data['data_expired'] == True
             return user
-        for field in __SETTINGS['SAVE_USER_FIELDS']:
+        for field in _SETTINGS['SAVE_USER_FIELDS']:
             setattr(user, field, data['msg'][field])
         return user
 
